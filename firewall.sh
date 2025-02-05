16775342df3163e31720c4087e04b3c8efbd2c49
Leonardo de Goes <secboyleo@gmail.com>
2025-02-04 22:48:20 -0300

    Add files via upload

---
+++ firewall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#script de firewall para icmp e ssh (stateful)
+#
+iptables -F
+iptables -X
+#
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+#
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+#
+iptables -A INPUT -p icmp --icmp-type echo-request -s 10.0.2.15/32 \
+	 -i eth0 -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 22 -s 10.0.2.15/32 -j ACCEPT
+
+#
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
